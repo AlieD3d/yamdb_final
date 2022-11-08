7cc11328930a1c026d9976751afe0b244385912b
Arteom Nesterow <utchi@DESKTOP-0LSV1VH>
2022-11-08 20:29:58 +0300

    try hard

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,11 +1,12 @@
 import os
 from datetime import timedelta
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG', False))
 
 ALLOWED_HOSTS = ['*', 'localhost']
 
@@ -60,16 +61,11 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME',
-                          default='postgres'),
-        'USER': os.getenv('POSTGRES_USER',
-                          default='mirai'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD',
-                              default='123lol'),
-        'HOST': os.getenv('DB_HOST',
-                          default='db'),
-        'PORT': os.getenv('DB_PORT',
-                          default='5432')
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
